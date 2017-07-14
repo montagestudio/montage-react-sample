8975ad257ec952c64925d1f5eb79e1caf94f82d3
Thibault Zanini <thibaultzanini@gmail.com>
2017-07-14 17:01:09 -0400

    Update demo with react bindings exemple.

--- src/App.js
+++ src/App.js
@@ -5,33 +5,53 @@ import './App.css';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { rangeValue: 50 };
-  }
+    constructor(props) {
+        super(props);
+        this.state = { rangeValue: 0 };
+        this.handleAttributeChange = this.handleAttributeChange.bind(this);
+    }
 
-  resetRange() {
-    this.setState(prevState => ({
-      rangeValue: 0
-    }));
-  }
+    resetRange() {
+        this.setState(prevState => ({
+            rangeValue: 0
+        }));
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          <montage-element data-module-id="ui/title.reel"></montage-element>
-          <montage-element data-module-id="ui/hello-world.reel"></montage-element>
-          <montage-element data-module-id="ui/bindings.reel" data-value={this.state.rangeValue}></montage-element>
-          <button onClick={(e) => this.resetRange(e)}>Reset</button>
-        </p>
-      </div>
-    );
-  }
+    handleAttributeChange(event) {
+        if (event.detail.attribute === "data-value") {
+            this.setState(prevState => ({
+                rangeValue: event.detail.newValue
+            }));
+        }
+    }
+
+    componentDidMount() {
+        this.app.addEventListener("dataAttributeChange", this.handleAttributeChange);
+    }
+
+    componentWillUnmount() {
+        this.app.removeEventListener("dataAttributeChange", this.handleAttributeChange);
+    }
+
+    render() {
+        let rangeValue = this.state.rangeValue;
+
+        return (
+            <div className="App" ref={(app) => { this.app = app; }}>
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <div className="App-intro">
+                    <montage-element data-module-id="ui/title.reel"></montage-element>
+                    <montage-element data-module-id="ui/hello-world.reel"></montage-element>
+                    <h4>How many YEAH? : {rangeValue}</h4>
+                    <montage-element data-module-id="ui/bindings.reel" data-value={rangeValue}></montage-element>
+                    <button onClick={(e) => this.resetRange(e)}>Reset</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
